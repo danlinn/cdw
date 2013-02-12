9eb5312ce422c157ef52ddbc0f4d39c30192f6ca
Daniel Linn <dan@metaltoad.com>
2013-02-11 16:08:29 -0800

    site updated

--- drupal/sites/all/modules/google_analytics/googleanalytics.js
+++ drupal/sites/all/modules/google_analytics/googleanalytics.js
@@ -1,6 +1,8 @@
-// $Id: googleanalytics.js,v 1.9.2.4 2010/09/19 11:39:20 hass Exp $
 
 $(document).ready(function() {
+
+  // Expression to check for absolute internal links.
+  var isInternal = new RegExp("^(https?):\/\/" + window.location.host, "i");
 
   // Attach onclick event to document only and catch clicks on all elements.
   $(document.body).click(function(event) {
@@ -8,8 +10,6 @@ $(document).ready(function() {
     $(event.target).parents("a:first,area:first").andSelf().filter("a,area").each(function() {
 
       var ga = Drupal.settings.googleanalytics;
-      // Expression to check for absolute internal links.
-      var isInternal = new RegExp("^(https?):\/\/" + window.location.host, "i");
       // Expression to check for special links like gotwo.module /go/* links.
       var isInternalSpecial = new RegExp("(\/go\/.*)$", "i");
       // Expression to check for download links.
@@ -17,8 +17,12 @@ $(document).ready(function() {
 
       // Is the clicked URL internal?
       if (isInternal.test(this.href)) {
+        // Skip 'click' tracking, if custom tracking events are bound.
+        if ($(this).is('.colorbox')) {
+          // Do nothing here. The custom event will handle all tracking.
+        }
         // Is download tracking activated and the file extension configured for download tracking?
-        if (ga.trackDownload && isDownload.test(this.href)) {
+        else if (ga.trackDownload && isDownload.test(this.href)) {
           // Download link clicked.
           var extension = isDownload.exec(this.href);
           _gaq.push(["_trackEvent", "Downloads", extension[1].toUpperCase(), this.href.replace(isInternal, '')]);
@@ -29,16 +33,25 @@ $(document).ready(function() {
         }
       }
       else {
-        if (ga.trackMailto && $(this).is("a[href^=mailto:],area[href^=mailto:]")) {
+        if (ga.trackMailto && $(this).is("a[href^='mailto:'],area[href^='mailto:']")) {
           // Mailto link clicked.
           _gaq.push(["_trackEvent", "Mails", "Click", this.href.substring(7)]);
         }
-        else if (ga.trackOutgoing && this.href) {
+        else if (ga.trackOutbound && this.href.match(/^\w+:\/\//i)) {
           // External link clicked.
-          _gaq.push(["_trackEvent", "Outgoing links", "Click", this.href]);
+          _gaq.push(["_trackEvent", "Outbound links", "Click", this.href]);
         }
       }
-
     });
   });
+
+  // Colorbox: This event triggers when the transition has completed and the
+  // newly loaded content has been revealed.
+  $(document).bind("cbox_complete", function() {
+    var href = $.colorbox.element().attr("href");
+    if (href) {
+      _gaq.push(["_trackPageview", href.replace(isInternal, '')]);
+    }
+  });
+
 });
